4495fc7c8322f548bfb65cdf2a23352e36ea8e2a
Balog Zoltan <Balog Zoltan>
2017-03-02 11:49:41 +0100

    v0.0.2

--- froala-bf.module.ts
+++ froala-bf.module.ts
@@ -1,13 +1,11 @@
 import { NgModule }           from "@angular/core";
 
-import { SharedModule }       from "../shared_modules/shared.module";
 import { FroalaEditorModule } from "angular2-froala-wysiwyg";
 
 import { FroalaBfDirectives } from "./froala-bf.directives";
 
 @NgModule({
     imports: [
-        SharedModule,
         FroalaEditorModule.forRoot()
     ],
     declarations: [
